9d11147ebc6e57d59e8299449b2632da080eb0b0
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-01-04 01:38:02 -0300

    test: testing books page

---
+++ cypress/e2e/books.spec.cy.ts
@@ -0,0 +1,31 @@
+import { Book } from '@/entities/Book'
+import { BookPropsMock } from '@/tests/mocks/book.mock'
+
+describe('Books', () => {
+	beforeEach(() => cy.visit('/books'))
+
+	it('should present books', () => {
+		cy.getByTestId('books-list').should('be.visible')
+		cy.getByTestId('book-item').should('have.length', 7)
+	})
+
+	it('should navigate to details page when click on book title', () => {
+		const book = new Book({
+			...BookPropsMock,
+		})
+
+		cy.getByTestId('book-item').first().find('a').first().click()
+
+		cy.location('pathname').should('contain', `/books/${book.id}`)
+	})
+
+	it('should navigate to details page when click on book image', () => {
+		const book = new Book({
+			...BookPropsMock,
+		})
+
+		cy.getByTestId('book-item').first().find('a').last().click()
+
+		cy.location('pathname').should('contain', `/books/${book.id}`)
+	})
+})
