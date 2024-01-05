5c2725f00344adfe6575882db81ddd69245e0819
Eduardo Nogueira <eduardodudunog@gmail.com>
2024-01-05 14:01:42 -0300

    feat: add spell not found page

--- src/app/spells/[id]/page.tsx
+++ src/app/spells/[id]/page.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import { Fragment, useMemo } from 'react'
-import Image from 'next/image'
+import { Fragment } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import { useGetSpell } from '@/app/spells/hooks/use-get-spell'
-import { Badge } from '@/shared/components/ui/badge'
 import { Button } from '@/shared/components/ui/button'
 import { Skeleton } from '@/shared/components/ui/skeleton'
 import {
@@ -13,8 +11,8 @@ import {
 	CardContent,
 	CardFooter,
 	CardHeader,
-	CardTitle,
 } from '@/shared/components/ui/card'
+import { SpellDetails } from '@/app/spells/components/spell-details'
 import { FaArrowRightLong } from 'react-icons/fa6'
 import { GiFairyWand } from 'react-icons/gi'
 
@@ -61,111 +59,34 @@ export default function Spell() {
 		id: String(spellId),
 	})
 
-	const hasData = useMemo(() => spell && (spell.name || spell?.effect), [spell])
-
 	return (
-		<Card
-			key={spell?.id}
-			className="my-4 bg-secondary border-green max-w-[52rem] w-full"
-		>
-			{isLoading ? (
-				<SpellSkeleton />
-			) : (
-				<Fragment>
-					<CardHeader className="flex items-center gap-4 justify-between xs:flex-row">
-						<CardTitle className="mt-2 text-green font-bold w-full">
-							{spell?.name}
-						</CardTitle>
-						<Link
-							href={String(spell?.wiki)}
-							target="_blank"
-							className="flex items-center gap-3"
-						>
+		<Fragment>
+			{isLoading && (
+				<Card className="my-4 bg-secondary border-green max-w-[52rem] w-full">
+					<SpellSkeleton />
+				</Card>
+			)}
+
+			{spell && <SpellDetails spell={spell} />}
+
+			{!spell && !isLoading && (
+				<Card className="my-6 bg-secondary border-green max-w-[52rem] w-full">
+					<div className="my-8 flex flex-col items-center text-center text-green font-bold">
+						<GiFairyWand size={150} />
+						<p className="mt-6">Nenhum feitiço encontrada</p>
+
+						<Link href="/spells" className="mt-10 flex items-center gap-3">
 							<Button
 								variant="default"
 								className="mt-0 flex items-center gap-3 bg-green"
 							>
-								Wiki
+								Ir para a página de feitiços
 								<FaArrowRightLong />
 							</Button>
 						</Link>
-					</CardHeader>
-					<CardContent className="flex flex-col md:flex-row gap-8">
-						<div className="flex flex-col items-center gap-6">
-							{spell?.image ? (
-								<Image
-									width={200}
-									height={200}
-									src={String(spell?.image)}
-									className="transition-all hover:scale-105"
-									alt="Spell image"
-								/>
-							) : (
-								<GiFairyWand size="100%" />
-							)}
-
-							{spell?.category && (
-								<Badge className="mt-2 bg-green">{spell?.category}</Badge>
-							)}
-						</div>
-
-						{hasData && (
-							<div className="flex flex-col gap-4">
-								{spell?.effect && (
-									<div className="space-y-1 max-w-[35rem]">
-										<p className="text-sm text-green font-bold leading-none">
-											Efeito
-										</p>
-										<p className="text-sm text-muted-foreground">
-											{spell?.effect}
-										</p>
-									</div>
-								)}
-								{spell?.creator && (
-									<div className="space-y-1 max-w-[35rem]">
-										<p className="text-sm text-green font-bold leading-none">
-											Criador
-										</p>
-										<p className="text-sm text-muted-foreground">
-											{spell?.creator}
-										</p>
-									</div>
-								)}
-								{spell?.light && (
-									<div className="space-y-1 max-w-[35rem]">
-										<p className="text-sm text-green font-bold leading-none">
-											Luz
-										</p>
-										<p className="text-sm text-muted-foreground">
-											{spell?.light}
-										</p>
-									</div>
-								)}
-								{spell?.hand && (
-									<div className="space-y-1 max-w-[35rem]">
-										<p className="text-sm text-green font-bold leading-none">
-											Comando
-										</p>
-										<p className="text-sm text-muted-foreground">
-											{spell?.hand}
-										</p>
-									</div>
-								)}
-							</div>
-						)}
-
-						{!hasData && (
-							<div className="mt-6">
-								<p className="text-base text-green leading-none">
-									Não há dados para este feitiço
-								</p>
-							</div>
-						)}
-					</CardContent>
-
-					<CardFooter />
-				</Fragment>
+					</div>
+				</Card>
 			)}
-		</Card>
+		</Fragment>
 	)
 }
---
+++ src/app/spells/components/spell-details/spell-details.tsx
@@ -0,0 +1,113 @@
+import { useMemo } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Spell } from '@/entities/Spell'
+import { Button } from '@/shared/components/ui/button'
+import { Badge } from '@/shared/components/ui/badge'
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/shared/components/ui/card'
+import { FaArrowRightLong } from 'react-icons/fa6'
+import { GiFairyWand } from 'react-icons/gi'
+
+type SpellDetailsProps = {
+	spell: Spell
+}
+
+const SpellDetails = ({ spell }: SpellDetailsProps) => {
+	const hasData = useMemo(() => spell && (spell.name || spell?.effect), [spell])
+
+	return (
+		<Card className="my-4 bg-secondary border-green max-w-[52rem] w-full">
+			<CardHeader className="flex items-center gap-4 justify-between xs:flex-row">
+				<CardTitle className="mt-2 text-green font-bold w-full">
+					{spell?.name}
+				</CardTitle>
+				<Link
+					href={String(spell?.wiki)}
+					target="_blank"
+					className="flex items-center gap-3"
+				>
+					<Button
+						variant="default"
+						className="mt-0 flex items-center gap-3 bg-green"
+					>
+						Wiki
+						<FaArrowRightLong />
+					</Button>
+				</Link>
+			</CardHeader>
+			<CardContent className="flex flex-col md:flex-row gap-8">
+				<div className="flex flex-col items-center gap-6">
+					{spell?.image ? (
+						<Image
+							width={200}
+							height={200}
+							src={String(spell?.image)}
+							className="transition-all hover:scale-105"
+							alt="Spell image"
+						/>
+					) : (
+						<GiFairyWand size="100%" />
+					)}
+
+					{spell?.category && (
+						<Badge className="mt-2 bg-green">{spell?.category}</Badge>
+					)}
+				</div>
+
+				{hasData && (
+					<div className="flex flex-col gap-4">
+						{spell?.effect && (
+							<div className="space-y-1 max-w-[35rem]">
+								<p className="text-sm text-green font-bold leading-none">
+									Efeito
+								</p>
+								<p className="text-sm text-muted-foreground">{spell?.effect}</p>
+							</div>
+						)}
+						{spell?.creator && (
+							<div className="space-y-1 max-w-[35rem]">
+								<p className="text-sm text-green font-bold leading-none">
+									Criador
+								</p>
+								<p className="text-sm text-muted-foreground">
+									{spell?.creator}
+								</p>
+							</div>
+						)}
+						{spell?.light && (
+							<div className="space-y-1 max-w-[35rem]">
+								<p className="text-sm text-green font-bold leading-none">Luz</p>
+								<p className="text-sm text-muted-foreground">{spell?.light}</p>
+							</div>
+						)}
+						{spell?.hand && (
+							<div className="space-y-1 max-w-[35rem]">
+								<p className="text-sm text-green font-bold leading-none">
+									Comando
+								</p>
+								<p className="text-sm text-muted-foreground">{spell?.hand}</p>
+							</div>
+						)}
+					</div>
+				)}
+
+				{!hasData && (
+					<div className="mt-6">
+						<p className="text-base text-green leading-none">
+							Não há dados para este feitiço
+						</p>
+					</div>
+				)}
+			</CardContent>
+			<CardFooter />
+		</Card>
+	)
+}
+
+export { SpellDetails }
